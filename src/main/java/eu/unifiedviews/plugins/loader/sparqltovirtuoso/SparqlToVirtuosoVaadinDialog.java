46000d9a85f64bc74e860f560c04550140b12d56
Cristian Vasquez <cristian.vasquez@tenforce.com>
2016-03-29 14:28:59 +0200

    Cleanup

--- src/main/java/eu/unifiedviews/plugins/loader/sparqltovirtuoso/SparqlToVirtuosoVaadinDialog.java
+++ src/main/java/eu/unifiedviews/plugins/loader/sparqltovirtuoso/SparqlToVirtuosoVaadinDialog.java
@@ -1,14 +1,9 @@
 package eu.unifiedviews.plugins.loader.sparqltovirtuoso;
 
-import com.vaadin.data.Validator;
 import com.vaadin.data.util.ObjectProperty;
-import com.vaadin.ui.FormLayout;
-import com.vaadin.ui.PasswordField;
-import com.vaadin.ui.TextArea;
-import com.vaadin.ui.TextField;
+import com.vaadin.ui.*;
 import eu.unifiedviews.dpu.config.DPUConfigException;
 import eu.unifiedviews.helpers.dpu.vaadin.dialog.AbstractDialog;
-import eu.unifiedviews.helpers.dpu.vaadin.dialog.UserDialogContext;
 
 /**
  * DPU's configuration dialog. User can use this dialog to configure DPU
@@ -30,40 +25,36 @@ public class SparqlToVirtuosoVaadinDialog extends AbstractDialog<SparqlToVirtuos
         super(SparqlToVirtuoso.class);
     }
 
-    public void outerBuildDialogLayout(UserDialogContext ctx) {
-        this.ctx = ctx;
-        buildDialogLayout();
-    }
-
     @Override
     protected void buildDialogLayout() {
         setSizeFull();
-        FormLayout mainLayout = new FormLayout();
 
-        // top-level component properties
-        setWidth("100%");
-        setHeight("100%");
+        final VerticalLayout mainLayout = new VerticalLayout();
+        mainLayout.setImmediate(false);
+        mainLayout.setSizeFull();
+        mainLayout.setSpacing(true);
+        mainLayout.setMargin(true);
 
         final PasswordField passwordField = new PasswordField(ctx.tr("SparqlToVirtuosoVaadinDialog.password"), password);
-        passwordField.setWidth("100%");
+        passwordField.setWidth(100, Unit.PERCENTAGE);
 
         mainLayout.addComponent(createTextField(ctx.tr("SparqlToVirtuosoVaadinDialog.virtuosoUrl"), virtuosoUrl));
         mainLayout.addComponent(createTextField(ctx.tr("SparqlToVirtuosoVaadinDialog.username"), username));
         mainLayout.addComponent(passwordField);
 
-        txtQuery = new TextArea(ctx.tr("SparqlConstructVaadinDialog.constructQuery"));
+        txtQuery = new TextArea();
         txtQuery.setSizeFull();
-        txtQuery.addValidator(createSparqlQueryValidator());
+        txtQuery.setCaption(ctx.tr("SparqlConstructVaadinDialog.constructQuery"));
         txtQuery.setImmediate(true);
         mainLayout.addComponent(txtQuery);
-        mainLayout.setExpandRatio(txtQuery, 1);
 
+        mainLayout.setExpandRatio(txtQuery, 1);
         setCompositionRoot(mainLayout);
     }
 
     private <T> TextField createTextField(String caption, ObjectProperty<T> property) {
         final TextField result = new TextField(caption, property);
-        result.setWidth("100%");
+        result.setWidth(100, Unit.PERCENTAGE);
         return result;
     }
 
@@ -79,12 +70,8 @@ public class SparqlToVirtuosoVaadinDialog extends AbstractDialog<SparqlToVirtuos
     public SparqlToVirtuosoConfig getConfiguration() throws DPUConfigException {
         SparqlToVirtuosoConfig conf = new SparqlToVirtuosoConfig();
 
-
         if (txtQuery.getValue().isEmpty()) {
             throw new DPUConfigException(ctx.tr("SparqlConstructVaadinDialog.emptyQuery"));
-        }
-        if (!txtQuery.isValid()) {
-            throw new DPUConfigException(ctx.tr("sparqlvalidator.invalidQuery"));
         }
         conf.setQuery(txtQuery.getValue());
         conf.setVirtuosoUrl(virtuosoUrl.getValue());
@@ -92,27 +79,6 @@ public class SparqlToVirtuosoVaadinDialog extends AbstractDialog<SparqlToVirtuos
         conf.setPassword(password.getValue());
 
         return conf;
-    }
-
-    private Validator createSparqlQueryValidator() {
-        Validator validator = new Validator() {
-            private static final long serialVersionUID = 1L;
-
-            @Override
-            public void validate(Object value) throws InvalidValueException {
-                final String valueStr = (String) value;
-                if (value == null || valueStr.isEmpty()) {
-                    throw new InvalidValueException(ctx.tr("sparqlvalidator.emptyQuery"));
-                }
-//
-//                try {
-//                    QueryParserUtil.parseQuery(QueryLanguage.SPARQL, valueStr, null);
-//                } catch (MalformedQueryException ex) {
-//                    throw new InvalidValueException(ctx.tr("sparqlvalidator.invalidQuery") + " " + ex.getMessage());
-//                }
-            }
-        };
-        return validator;
     }
 
 }
